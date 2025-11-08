9fef277fcd97d9d0b7f6c8d0eab4f31344d085d2
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-09 00:03:18 +0900

    Style: 타입 정의

---
+++ src/shared/types/theme.d.ts
@@ -0,0 +1,63 @@
+export type ThemeName = 'light' | 'dark';
+
+export type ColorKey =
+  | 'primary'
+  | 'secondary'
+  | 'background'
+  | 'secondText'
+  | 'border'
+  | 'gradationGreen'
+  | 'errorText'
+  | 'white'
+  | 'black'
+  | 'hoverOverlay'
+  | 'buttonActive';
+
+export type ShadowKey = 'default';
+
+export type FontSizeKey = 'xlarge' | 'large' | 'medium' | 'small' | 'xsmall';
+
+export type ButtonSize =
+  | 'large'
+  | 'medium'
+  | 'small';
+
+export type ButtonScheme =
+  | 'primary'
+  | 'secondary'
+  | 'option'
+  | 'optionActive'
+  | 'prev'
+  | 'answer';
+
+export type BorderRadiusKey = 'small' | 'medium' | 'large' | 'round';
+
+export type Layout = 'large' | 'medium' | 'small';
+
+export type LayoutWidth = Layout;
+
+export type MediaQuery = 'mobile' | 'tablet' | 'desktop';
+
+export type ButtonSchemeVariant = {
+  color?: string;
+  backgroundColor: string;
+  border?: string;
+  boxShadow?: string;
+};
+
+export interface Theme {
+  name: ThemeName;
+  color: Record<ColorKey, string>;
+  shadow: Record<ShadowKey, string>;
+  fontSize: Record<FontSizeKey, string>;
+  buttonSize: Record<
+    ButtonSize,
+    { fontSize?: string; width?: string; padding: string }
+  >;
+  buttonScheme: Record<ButtonScheme, ButtonSchemeVariant>;
+  borderRadius: Record<BorderRadiusKey, string>;
+  layout: {
+    width: Record<LayoutWidth, string>;
+  };
+  mediaQuery: Record<MediaQuery, string>;
+}
