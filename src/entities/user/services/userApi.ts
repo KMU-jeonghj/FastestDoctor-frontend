9e40b5d423ad652b433586947b4f468bed28560c
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-11 00:48:45 +0900

    Feat: 함수명 수정

--- src/entities/user/services/userApi.ts
+++ src/entities/user/services/userApi.ts
@@ -1,7 +1,7 @@
 import { httpClient } from "../../../shared/api/httpClient";
 import type { UserType } from "../types/user.type";
 
-export const getResult = async (data: UserType)  => {
+export const registerUser = async (data: UserType) => {
   const response = await httpClient.post('/user', { params: data });
   return response.data;
 };
