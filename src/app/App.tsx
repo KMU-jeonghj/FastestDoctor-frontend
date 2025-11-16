a359d805636b96b50eeed8754930e36a8b83e702
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-17 03:22:23 +0900

    Feat: 페이지 라우팅, queryClient 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,22 @@
-
+import { QueryClientProvider } from "@tanstack/react-query"
+import { Routers } from "./Routers"
 import { ThemeProviders } from "./ThemeProvider"
+import { queryClient } from "shared/lib/queryClient"
+import { BrowserRouter } from "react-router-dom"
+import ScrollToTop from "shared/ui/ScrollToTop/ScrollToTop"
 
 function App() {
 
 
   return (
-    <ThemeProviders>
-      <div>
-        hello world
-      </div>
-
-    </ThemeProviders>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProviders>
+        <BrowserRouter>
+          <ScrollToTop />
+          <Routers />
+        </BrowserRouter>
+      </ThemeProviders>
+    </QueryClientProvider>
   )
 }
 
