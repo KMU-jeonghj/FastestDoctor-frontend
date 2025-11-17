560c714c46d142c17f58ccc00ebd50890b1d093d
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-18 00:59:43 +0900

    Feat: Button 컴포넌트 작성

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,70 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import { hoverOverlay } from 'shared/styles/hoverOverlay';
+import {
+  BorderRadiusKey,
+  ButtonScheme,
+  ButtonSize,
+  FontSizeKey,
+} from 'shared/types/theme';
+import styled from 'styled-components';
+
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  buttonSize: ButtonSize;
+  fontSize: FontSizeKey;
+  scheme: ButtonScheme;
+  borderRadius: BorderRadiusKey;
+}
+
+const Button = ({
+  children,
+  buttonSize,
+  fontSize,
+  scheme,
+  borderRadius = 'medium',
+  onClick,
+
+}: Props) => {
+  return (
+    <ButtonStyle
+      buttonSize={buttonSize}
+      fontSize={fontSize}
+      scheme={scheme}
+      onClick={onClick}
+      borderRadius={borderRadius}
+    >
+      {children}
+    </ButtonStyle>
+  );
+};
+
+const ButtonStyle = styled.button.withConfig({
+  shouldForwardProp: (prop) =>
+    ![
+      'scheme',
+      'buttonSize',
+      'fontSize',
+      'borderRadius',
+      'disableHoverOverlay',
+      '$active',
+    ].includes(prop),
+}) <Omit<Props, 'children'>>`
+  font-size: ${({ theme, buttonSize, fontSize }) => (theme.buttonSize[buttonSize].fontSize ? theme.buttonSize[buttonSize].fontSize : theme.fontSize[fontSize])};
+  padding: ${({ theme, buttonSize }) => theme.buttonSize[buttonSize].padding};
+  width: ${({ theme, buttonSize }) => (theme.buttonSize[buttonSize].width ? theme.buttonSize[buttonSize].width : 'auto')};
+  color: ${({ theme, scheme }) => theme.buttonScheme[scheme].color};
+  background-color: ${({ theme, scheme }) =>
+    theme.buttonScheme[scheme].backgroundColor};
+  font-weight: 'normal';
+  border-radius: ${({ theme, borderRadius }) => theme.borderRadius[borderRadius]};
+  border: ${({ theme, scheme }) =>
+    theme.buttonScheme[scheme].border
+      ? `1px solid ${theme.buttonScheme[scheme].border}`
+      : 'none'};
+    line-height: 1;
+  
+  ${hoverOverlay}
+`;
+
+export default Button;
