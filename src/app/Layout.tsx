54cbaf0f498babff2963307b1797cac39c037603
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-23 23:01:44 +0900

    Style: 레이아웃 설정

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,10 +1,29 @@
 
 import { Outlet } from 'react-router-dom';
+import styled from 'styled-components';
+import Header from 'widgets/common/Header';
 
 
 export default function Layout() {
   return (
-    <Outlet />
+    <BackgroundStyle>
+      <Header />
+      <LayoutStyle>
+        <Outlet />
+      </LayoutStyle>
+    </BackgroundStyle>
+
   );
 }
+
+const LayoutStyle = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  padding: 20px 0;
+`;
+
+const BackgroundStyle = styled.div`
+  /* background-color: ${({ theme }) => theme.color.background}; */
+`;
 
---
+++ src/widgets/common/Header.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Logo from 'shared/ui/Logo/Logo';
+import Title from 'shared/ui/Title/Title';
+import styled from 'styled-components'
+
+const Header = () => {
+  return (
+    <HeaderStyle>
+      <Link to='/userinfo'>
+        <div className='logo-box'>
+          <Logo type='square' />
+          <div className='title'>
+            <Title fontSize='large' fontWeight='semibold'>세빠닥</Title>
+            <Title fontSize='xsmall' fontWeight='medium' color='secondText'>세상에서 제일 빠른 닥터</Title>
+          </div>
+        </div>
+      </Link>
+
+    </HeaderStyle>
+  )
+}
+
+const HeaderStyle = styled.div`
+  width: 100%;
+  padding: 0.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.color.background};
+  border-bottom: 1px solid ${({ theme }) => theme.color.border};
+  cursor: pointer;
+  
+  .logo-box {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+`;
+
+export default Header
