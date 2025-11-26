1b6464cdfc40f865a11e94bfd8ea1007aa4331cf
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-26 23:23:41 +0900

    Feat: 배포 시 msw 임포트 제외

--- src/app/mockWorker.ts
+++ src/app/mockWorker.ts
@@ -1,9 +1,13 @@
-import { worker } from '../shared/mocks/browser';
 
 export const startMockWorker = async () => {
+  //DEV 모드 일 때만 -> MSW 실행
   if (import.meta.env.VITE_USE_MSW === 'true' && import.meta.env.DEV) {
+    // ▼ 여기서 import를 하면, 배포 시에는 이 코드가 아예 포함되지 않습니다 (Tree-shaking)
+    const { worker } = await import('../shared/mocks/browser');
+
     await worker.start({
-      onUnhandledRequest: 'bypass', // 백엔드로 요청 넘기기
+      onUnhandledRequest: 'bypass',
     });
   }
+  // 배포 환경이면 아무것도 하지 않고 함수 종료 (Promise.resolve)
 };
