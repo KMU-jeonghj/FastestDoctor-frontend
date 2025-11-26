cab55b79db66af144c2a969fe5cb53a98d1b5ef7
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-11-26 23:58:06 +0900

    Fix: user api 수정

--- src/entities/user/services/userApi.ts
+++ src/entities/user/services/userApi.ts
@@ -2,6 +2,6 @@ import { httpClient } from "../../../shared/api/httpClient";
 import type { UserType } from "../types/user.type";
 
 export const registerUser = async (data: UserType) => {
-  const response = await httpClient.post('/v1/users', { data });
+  const response = await httpClient.post('/v1/users', data);
   return response.data;
 };
